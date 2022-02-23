b0ad9ff6b6513765ffbf52848021d3d94e3a8e6e
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-02-23 14:42:33 -0500

    homePostCardChevronRotation

--- front-components/home-post-card.js
+++ front-components/home-post-card.js
@@ -1,8 +1,14 @@
 import styles from "../styles/Components.module.scss";
 import Image from "next/image";
 import HomePostCardExtension from "./home-post-card-extension";
+import React, { useState } from "react";
 
 export default function HomePostCard(props) {
+  const [rotateChevron, setRotateChevron] = useState(false);
+  function shExtend() {
+    setRotateChevron(!rotateChevron);
+  }
+
   return (
     <div className={styles["full-home-card"]}>
       <div className={styles["home-post-card"]}>
@@ -10,19 +16,23 @@ export default function HomePostCard(props) {
           <Image src="/bookmark-unselected.svg" alt="bookmark" layout="fill" />
         </div>
         <div className={styles["post-title"]}>{props.postName}</div>
-        <div className={styles["post-card-carot"]}>
+        <div
+          className={styles[`post-card-carot${rotateChevron ? "-rotate" : ""}`]}
+          onClick={() => shExtend()}
+        >
           <Image src="/arrow-right.png" alt="carot" layout="fill" />
         </div>
-        <div className={styles["subheader-list"]}>
-          {props.subheaderList.map((subheader) => {
-            return (
-              <HomePostCardExtension
-                subheader={subheader.SubheaderName}
-                key={subheader}
-              />
-            );
-          })}
-        </div>
+      </div>
+
+      <div className={styles["subheader-list"]}>
+        {props.subheaderList.map((subheader) => {
+          return (
+            <HomePostCardExtension
+              subheader={subheader.SubheaderName}
+              key={subheader}
+            />
+          );
+        })}
       </div>
     </div>
   );
