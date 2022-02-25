d63a293ee43883bcba59062d9efca5405dcf8c6b
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-02-25 12:46:21 -0500

    mobileMenu

--- front-components/header.js
+++ front-components/header.js
@@ -1,37 +1,49 @@
-import Link from 'next/link'
-import styles from '../styles/Components.module.scss';
-import Image from 'next/image';
+import Link from "next/link";
+import styles from "../styles/Components.module.scss";
+import Image from "next/image";
 
 export default function HeaderMenu() {
   return (
-    
-      <div className={styles['headerwrap']}>
-
-        <div className={styles['headerlogowrap']}>
-          <Link href="/"><img src="/acorn.svg" alt="Nutshell News Logo" className="headerlogo" style={{height:"35px",paddingLeft:"15px", paddingRight:"15px", margin:"0 auto"}}/></Link>
-          <Link href="/">
-            <h1 className={styles['headertitle']}>
-            nutshell news</h1>
-          </Link>
-        </div>
-
-        <ul className={styles['headerbuttonslist']} >
-          <li className={styles['headerbuttons']} ><Link href="/">News</Link></li>
-          <li className={styles['headerbuttons']}><Link href="/life">Life</Link></li>
-          <li className={styles['headerbuttons']}><Link href="/academic">Academic</Link></li>
-          <li className={styles['headerbuttons']}><Link href="/readinglist">Readling List</Link></li>
-          <li className={styles['headerbuttons']}><Link href="/about">About</Link></li>
-        </ul>
-
-        <div className={styles['headersearchwrap']} >
-          <span className={styles['search-icon-wrap']}>
-            <Image src="/searchicon.svg" alt="search" layout='fill'/>
-          </span>
-          <Link href="/search"><p className={styles['headersearchtext']}> Search</p></Link>
-        </div>
-        
-        <div className={styles['headerlogin']}>Login</div>
-
+    <div className={styles["headerwrap"]}>
+      <div className={styles["headerlogowrap"]}>
+        <Link href="/" passHref>
+          <p className={styles["headerlogo"]}>
+            <Image src="/acorn.svg" alt="Nutshell News Logo" layout="fill" />
+          </p>
+        </Link>
+        <Link href="/" passHref>
+          <h1 className={styles["headertitle"]}>nutshell news</h1>
+        </Link>
       </div>
-    
-)}
+
+      <ul className={styles["headerbuttonslist"]}>
+        <li className={styles["headerbuttons"]}>
+          <Link href="/">News</Link>
+        </li>
+        <li className={styles["headerbuttons"]}>
+          <Link href="/life">Life</Link>
+        </li>
+        <li className={styles["headerbuttons"]}>
+          <Link href="/academic">Academic</Link>
+        </li>
+        <li className={styles["headerbuttons"]}>
+          <Link href="/readinglist">Readling List</Link>
+        </li>
+        <li className={styles["headerbuttons"]}>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+
+      <div className={styles["headersearchwrap"]}>
+        <span className={styles["search-icon-wrap"]}>
+          <Image src="/searchicon.svg" alt="search" layout="fill" />
+        </span>
+        <Link href="/search">
+          <p className={styles["headersearchtext"]}> Search</p>
+        </Link>
+      </div>
+
+      <div className={styles["headerlogin"]}>Login</div>
+    </div>
+  );
+}
