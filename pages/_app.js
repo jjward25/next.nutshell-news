f947550e5054e85f27b2933e32dd009aad644aae
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-03-04 23:40:20 -0500

    noAWS

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import "../styles/globals.scss";
 import Layout from "../front-components/layout";
-import Amplify from "aws-amplify";
-import config from "../src/aws-exports";
-
-Amplify.configure({
-  ...config,
-  ssr: true,
-});
 
 function MyApp({ Component, pageProps }) {
   return (
